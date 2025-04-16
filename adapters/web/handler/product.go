70ee7919f1ea52f80d61e14b73c42e7e010db179
Flávio Dutra <flaviomdutra@outlook.com>
2025-04-15 23:10:09 -0300

    Add product DTO and implement createProduct handler for product creation

--- adapters/web/handler/product.go
+++ adapters/web/handler/product.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/codegangsta/negroni"
+	"github.com/flaviomdutra/product-srv-hexagonal/adapters/dto"
 	"github.com/flaviomdutra/product-srv-hexagonal/application"
 	"github.com/gorilla/mux"
 )
@@ -14,9 +15,8 @@ func MakeProductHandlers(
 	n *negroni.Negroni,
 	service application.ProductServiceInterface,
 ) {
-	r.Handle("/product/{id}", n.With(
-		negroni.Wrap(getProduct(service)),
-	)).Methods("GET", "OPTIONS")
+	r.Handle("/product/{id}", n.With(negroni.Wrap(getProduct(service)))).Methods("GET", "OPTIONS")
+	r.Handle("/product", n.With(negroni.Wrap(createProduct(service)))).Methods("POST", "OPTIONS")
 }
 
 func getProduct(service application.ProductServiceInterface) http.HandlerFunc {
@@ -31,6 +31,33 @@ func getProduct(service application.ProductServiceInterface) http.HandlerFunc {
 			w.WriteHeader(http.StatusNotFound)
 			return
 		}
+		err = json.NewEncoder(w).Encode(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+}
+
+func createProduct(service application.ProductServiceInterface) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		var productDTO dto.Product
+		err := json.NewDecoder(r.Body).Decode(&productDTO)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(jsonError(err.Error()))
+			return
+		}
+
+		product, err := service.Create(productDTO.Name, productDTO.Price)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(jsonError(err.Error()))
+			return
+		}
+
 		err = json.NewEncoder(w).Encode(product)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
